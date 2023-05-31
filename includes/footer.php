31d20f971d899e6d22e2f55786b03e12b67d1819
Adam Blakey <adam@blakey.family>
2023-05-31 18:07:33 +0100

    Updated source code link in footer.

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
     <div class="row text-center align-items-center flex-row-reverse">
       <div class="col-lg-auto ms-lg-auto">
         <ul class="list-inline list-inline-dots mb-0">
-          <li class="list-inline-item"><a href="https://github.com/JustAdamHere/bookererer" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
+          <li class="list-inline-item"><a href="https://gitlab.com/adam.blakey/bookererer" target="_blank" class="link-secondary" rel="noopener">Source code</a></li>
         </ul>
       </div>
       <div class="col-12 col-lg-auto mt-3 mt-lg-0">
