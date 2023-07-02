c8caaff9f2725e6bf21767f497c2606dd0aaf530
Adam Blakey <adam@blakey.family>
2023-07-02 22:15:30 +0100

    Status 5 bookings are ignored when calculating clashes.

--- includes/functions.php
+++ includes/functions.php
@@ -9473,51 +9473,54 @@ function test_clash($booking_ID, $start, $end)
   $clash_detected = false;
   while($booking = $all_bookings->fetch_assoc())
   {
-    $test_start_datetime = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_start_datetime->setTimestamp($booking["datetime"]);
-
-    $test_start_start_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_start_start_of_day->setDate($test_start_datetime->format("Y"), $test_start_datetime->format("m"), $test_start_datetime->format("d"));
-
-    $test_start_end_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_start_end_of_day->setTimestamp($test_start_start_of_day->getTimestamp());
-    $test_start_end_of_day->modify("+1 day");
-
-    $test_end_datetime = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_end_datetime->setTimestamp($booking["datetime_end"]);
-
-    $test_end_start_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_end_start_of_day->setDate($test_end_datetime->format("Y"), $test_end_datetime->format("m"), $test_end_datetime->format("d"));
-
-    $test_end_end_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
-    $test_end_end_of_day->setTimestamp($test_end_start_of_day->getTimestamp());
-    $test_end_end_of_day->modify("+1 day");
-
-    $start_clash_detected    = ($test_start_start_of_day->getTimestamp() <= $start_datetime->getTimestamp()) && ($start_datetime->getTimestamp() <= $test_end_end_of_day->getTimestamp());
-    $end_clash_detected      = ($test_start_start_of_day->getTimestamp() <= $end_datetime->getTimestamp())   && ($end_datetime->getTimestamp()   <= $test_end_end_of_day->getTimestamp());
-    $straddle_clash_detected = ($start_datetime->getTimestamp() <= $test_start_start_of_day->getTimestamp()) && ($test_end_end_of_day->getTimestamp() <= $end_datetime->getTimestamp());
-
-    // echo $test_start_start_of_day->format("Y-m-d H:i:s")."<br>";
-    // echo $start_datetime->format("Y-m-d H:i:s")."<br>";
-    // echo $test_end_end_of_day->format("Y-m-d H:i:s")."<br>";
-
-    // if ($start_clash_detected)
-    // {
-    //   "Start clash detected<br>";
-    // }
-    // if ($end_clash_detected)
-    // {
-    //   "End clash detected<br>";
-    // }
-    // if ($straddle_clash_detected)
-    // {
-    //   "Straddle clash detected<br>";
-    // }
-
-    if ($start_clash_detected || $end_clash_detected || $straddle_clash_detected)
+    if ($booking["status"] != 5)
     {
-      $clash_detected = true;
-      break;
+      $test_start_datetime = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_start_datetime->setTimestamp($booking["datetime"]);
+
+      $test_start_start_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_start_start_of_day->setDate($test_start_datetime->format("Y"), $test_start_datetime->format("m"), $test_start_datetime->format("d"));
+
+      $test_start_end_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_start_end_of_day->setTimestamp($test_start_start_of_day->getTimestamp());
+      $test_start_end_of_day->modify("+1 day");
+
+      $test_end_datetime = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_end_datetime->setTimestamp($booking["datetime_end"]);
+
+      $test_end_start_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_end_start_of_day->setDate($test_end_datetime->format("Y"), $test_end_datetime->format("m"), $test_end_datetime->format("d"));
+
+      $test_end_end_of_day = new DateTime("1970-01-01 00:00:00", new DateTimeZone("Europe/London"));
+      $test_end_end_of_day->setTimestamp($test_end_start_of_day->getTimestamp());
+      $test_end_end_of_day->modify("+1 day");
+
+      $start_clash_detected    = ($test_start_start_of_day->getTimestamp() <= $start_datetime->getTimestamp()) && ($start_datetime->getTimestamp() <= $test_end_end_of_day->getTimestamp());
+      $end_clash_detected      = ($test_start_start_of_day->getTimestamp() <= $end_datetime->getTimestamp())   && ($end_datetime->getTimestamp()   <= $test_end_end_of_day->getTimestamp());
+      $straddle_clash_detected = ($start_datetime->getTimestamp() <= $test_start_start_of_day->getTimestamp()) && ($test_end_end_of_day->getTimestamp() <= $end_datetime->getTimestamp());
+
+      // echo $test_start_start_of_day->format("Y-m-d H:i:s")."<br>";
+      // echo $start_datetime->format("Y-m-d H:i:s")."<br>";
+      // echo $test_end_end_of_day->format("Y-m-d H:i:s")."<br>";
+
+      // if ($start_clash_detected)
+      // {
+      //   "Start clash detected<br>";
+      // }
+      // if ($end_clash_detected)
+      // {
+      //   "End clash detected<br>";
+      // }
+      // if ($straddle_clash_detected)
+      // {
+      //   "Straddle clash detected<br>";
+      // }
+
+      if ($start_clash_detected || $end_clash_detected || $straddle_clash_detected)
+      {
+        $clash_detected = true;
+        break;
+      }
     }
   }
 
